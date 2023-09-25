a40d75279924fb3fa6c45d24354478fa6ec01175
ttli3 <ttli3@illinois.edu>
2023-09-25 16:11:40 -0500

    First commit, added react and flask setup and basic tests

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Data from './Test'; 
+
+
+function App() {
+  return (
+    <div>
+      <h1>THIS IS A TEST</h1>
+      <Data /> 
+    </div>
+  );
+}
+
+export default App;
+
---
+++ client/src/Test.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+function Data() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('/api/data');
+            const responseData = await response.json();
+            setData(responseData);
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <h2>Data from Flask:</h2>
+            {data ? <p>{data.message}</p> : <p>Loading...</p>}
+        </div>
+    );
+}
+
+export default Data;
