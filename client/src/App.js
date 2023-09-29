05bf0c4bd02f90980aa37e0ee8bb7e75488b1931
Misho Metodiev <mishman3004@gmail.com>
2023-09-29 15:55:44 -0500

    React router installed, Login and Home pages added

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
 import './App.css';
 import Data from './Test'; 
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 
 function App() {
   return (
-    <div>
-      <h1>MVP Fantasy</h1>
-      <Data /> 
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' exact Component={HomePage} />
+          <Route path='/login' exact Component={LoginPage} />
+          {/* <Route path='/about' exact Component={About} />
+          <Route path='/contact' exact Component={Contact} />
+          <Route path='/privacy-policy' exact Component={Privacy} /> */}
+        </Routes>
+      </Router>
+    </>
   );
 }
 
