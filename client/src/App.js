b645494f2cf2d2b15b61096d2eeefa1fb5d474aa
deangelis1210 <charlie.j.724@gmail.com>
2023-10-13 15:34:28 -0500

    fixed issue where user could get past login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
-import './App.css';
 import SignUpPage from './components/pages/SignUpPage';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuth } from './AuthContext';
 
 function App() {
+  const { user } = useAuth();
+
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path='/' exact Component={LoginPage} />
-          <Route path='/home' exact Component={HomePage} />
-          <Route path='/signup' exact Component={SignUpPage} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={user ? <Navigate to="/home" /> : <LoginPage />}
+        />
+        <Route
+          path="/home"
+          element={user ? <HomePage /> : <Navigate to="/" />}
+        />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </Router>
   );
 }
 
