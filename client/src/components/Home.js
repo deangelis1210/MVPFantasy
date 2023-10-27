36c6f39466b1bbeb472c7ac1496b8b6502b7ee87
deangelis1210 <charlie.j.724@gmail.com>
2023-10-27 16:00:22 -0500

    added sun moon transition

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -20,12 +20,27 @@ function Home() {
   const { isDarkMode, toggleDarkMode } = useDarkMode();
   const handleDarkModeToggle = () => {
     toggleDarkMode();
+    
+    // Button switching functionality from https://learnersbucket.com/tutorials/js-projects/day-night-toggle-switch-in-javascript/
+    const switchBox = document.querySelector(".sun-moon");
+    document.querySelector("input").addEventListener("change", (e) => {
+      const { checked } = e.target;   
+      if (checked) {
+        switchBox.classList.remove("move");
+      } else {
+        switchBox.classList.add("move");
+      }
+    });
   };
 
   return (
     <div className={isDarkMode ? 'dark-mode home-big-container' : 'home-big-container'}>
         <button onClick={handleClick} className='log-in-button'>Sign Out</button>
-        <button onClick={handleDarkModeToggle} className='log-in-button'>Toggle Dark Mode</button>
+        <div class="sun-moon" onClick={handleDarkModeToggle}>
+          <input type="checkbox" />
+          <span class="circle large"></span>
+          <span class="circle small"></span>
+        </div>
       <div className="home-container">
         <img src='/images/logo.jpg' alt='logo' className='home-logo'/>
         <br/><br/>
