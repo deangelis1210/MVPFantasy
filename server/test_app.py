e06e6f90ae0558b52c25f342a957cb22cb295d9a
vinaySIllinois <85763354+vinaySIllinois@users.noreply.github.com>
2023-12-01 15:23:42 -0600

    Updated testing

--- server/test_app.py
+++ server/test_app.py
@@ -9,26 +9,32 @@ class TestApp(unittest.TestCase):
     def test_get_players_by_position(self):
         response = self.app.get('/api/get_players_by_position?position=QB')
         data = response.get_json()
-        self.assertEqual(response.status_code, 200, 'Expected response code 200, received {0}'.format(response.status_code))
+        self.assertEqual(response.status_code, 200, f'Expected response code 200, received {response.status_code}')
         self.assertIn('players', data, 'Expected response to contain players key')
 
     def test_get_player_names(self):
         response = self.app.get('/api/player-names')
         data = response.get_json()
-        self.assertEqual(response.status_code, 200, 'Expected response code 200, received {0}'.format(response.status_code))
+        self.assertEqual(response.status_code, 200, f'Expected response code 200, received {response.status_code}')
         self.assertIn('player_names', data, 'Expected response to contain player_names key')
 
     def test_search_player(self):
         response = self.app.get('/api/search_player?name=Patrick')
         data = response.get_json()
-        self.assertEqual(response.status_code, 200, 'Expected response code 200, received {0}'.format(response.status_code))
+        self.assertEqual(response.status_code, 200, f'Expected response code 200, received {response.status_code}')
         self.assertIn('players', data, 'Expected response to contain players key')
+
+        # Additional assertion for specific values
+        self.assertIn('Patrick Mahomes', data['players'], 'Expected Patrick Mahomes in the list of players')
 
     def test_get_player_stats(self):
         response = self.app.get('/api/get_player_stats?name=Patrick Mahomes')
         data = response.get_json()
-        self.assertEqual(response.status_code, 200, 'Expected response code 200, received {0}'.format(response.status_code))
+        self.assertEqual(response.status_code, 200, f'Expected response code 200, received {response.status_code}')
         self.assertIn('stats', data, 'Expected response to contain stats key')
+
+        # Additional assertion for specific values
+        self.assertEqual(data['stats']['Player'], 'Patrick Mahomes', 'Expected Player name to be Patrick Mahomes')
 
 if __name__ == '__main__':
     loader = unittest.TestLoader()
